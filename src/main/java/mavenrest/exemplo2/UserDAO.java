c9c725ef3e089297e72559e7ea4efb1461d57353
PedroSiqueira <pedro.siqueira@ifms.edu.br>
2023-07-26 15:48:46 -0400

    Changes to be committed: 	deleted:    nb-configuration.xml 	modified:   src/main/java/mavenrest/exemplo2/UserDAO.java 	modified:   src/main/java/mavenrest/exemplo3/UsuarioDAO.java
    resolving warnings

--- src/main/java/mavenrest/exemplo2/UserDAO.java
+++ src/main/java/mavenrest/exemplo2/UserDAO.java
@@ -3,10 +3,11 @@ package mavenrest.exemplo2;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
 import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 import javax.transaction.Transactional;
 
 public class UserDAO {
@@ -15,7 +16,7 @@ public class UserDAO {
     EntityManager em;
 
     public List<User> getAllUsers() {
-        return em.createQuery("SELECT u from User u").getResultList();
+        return em.createQuery("SELECT u from User u", User.class).getResultList();
     }
 
     public User getUser(long id) {
@@ -24,25 +25,27 @@ public class UserDAO {
 
     public User getUserByEmail(String email) {
         try {
-            return (User) em.createQuery("SELECT u from User u WHERE u.email = ?1").setParameter(1, email).getSingleResult();
+            return (User) em.createQuery("SELECT u from User u WHERE u.email = ?1").setParameter(1, email)
+                    .getSingleResult();
         } catch (NoResultException e) {
             return null;
         }
     }
 
     @Transactional
-    public void createUser(String nome, String email, String senha) throws NoSuchAlgorithmException, InvalidKeySpecException {
+    public void createUser(String nome, String email, String senha)
+            throws NoSuchAlgorithmException, InvalidKeySpecException {
         User user = new User(nome, email, senha);
         em.persist(user);
     }
 
     public List<User> search(String termo, String valor) {
-        Query query;
+        TypedQuery<User> query;
         if (termo.equals("id")) {
-            query = em.createQuery("SELECT u FROM User u WHERE u.id = ?1");
+            query = em.createQuery("SELECT u FROM User u WHERE u.id = ?1", User.class);
             query.setParameter(1, Long.valueOf(valor));
         } else {
-            query = em.createQuery("SELECT u FROM User u WHERE u." + termo + " LIKE ?1");
+            query = em.createQuery("SELECT u FROM User u WHERE u." + termo + " LIKE ?1", User.class);
             query.setParameter(1, "%" + valor + "%");
         }
         return query.getResultList();
@@ -54,7 +57,8 @@ public class UserDAO {
     }
 
     @Transactional
-    public void updateUser(long id, String nome, String email, String senha) throws NoSuchAlgorithmException, InvalidKeySpecException {
+    public void updateUser(long id, String nome, String email, String senha)
+            throws NoSuchAlgorithmException, InvalidKeySpecException {
         User u = em.find(User.class, id);
         u.setEmail(email);
         u.setName(nome);
--- src/main/java/mavenrest/exemplo3/UsuarioDAO.java
+++ src/main/java/mavenrest/exemplo3/UsuarioDAO.java
@@ -1,15 +1,17 @@
 package mavenrest.exemplo3;
 
-import authrest.AuthUser;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
 import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 import javax.transaction.Transactional;
+
 import authrest.AuthDAO;
+import authrest.AuthUser;
 
 public class UsuarioDAO implements AuthDAO {
 
@@ -17,7 +19,7 @@ public class UsuarioDAO implements AuthDAO {
     EntityManager em;
 
     public List<Usuario> getAllUsers() {
-        return em.createQuery("SELECT u from Usuario u").getResultList();
+        return em.createQuery("SELECT u from Usuario u", Usuario.class).getResultList();
     }
 
     public Usuario getUser(long id) {
@@ -26,25 +28,27 @@ public class UsuarioDAO implements AuthDAO {
 
     public Usuario getUserByEmail(String email) {
         try {
-            return (Usuario) em.createQuery("SELECT u from Usuario u WHERE u.email = ?1").setParameter(1, email).getSingleResult();
+            return (Usuario) em.createQuery("SELECT u from Usuario u WHERE u.email = ?1").setParameter(1, email)
+                    .getSingleResult();
         } catch (NoResultException e) {
             return null;
         }
     }
 
     @Transactional
-    public void createUser(String nome, String email, String senha, String permissao) throws NoSuchAlgorithmException, InvalidKeySpecException {
+    public void createUser(String nome, String email, String senha, String permissao)
+            throws NoSuchAlgorithmException, InvalidKeySpecException {
         Usuario user = new Usuario(nome, email, senha, permissao);
         em.persist(user);
     }
 
     public List<Usuario> search(String termo, String valor) {
-        Query query;
+        TypedQuery<Usuario> query;
         if (termo.equals("id")) {
-            query = em.createQuery("SELECT u FROM Usuario u WHERE u.id = ?1");
+            query = em.createQuery("SELECT u FROM Usuario u WHERE u.id = ?1", Usuario.class);
             query.setParameter(1, Long.valueOf(valor));
         } else {
-            query = em.createQuery("SELECT u FROM Usuario u WHERE u." + termo + " LIKE ?1");
+            query = em.createQuery("SELECT u FROM Usuario u WHERE u." + termo + " LIKE ?1", Usuario.class);
             query.setParameter(1, "%" + valor + "%");
         }
         return query.getResultList();
@@ -56,7 +60,8 @@ public class UsuarioDAO implements AuthDAO {
     }
 
     @Transactional
-    public void updateUser(long id, String nome, String email, String senha, String permissao) throws NoSuchAlgorithmException, InvalidKeySpecException {
+    public void updateUser(long id, String nome, String email, String senha, String permissao)
+            throws NoSuchAlgorithmException, InvalidKeySpecException {
         Usuario u = em.find(Usuario.class, id);
         u.setEmail(email);
         u.setName(nome);
